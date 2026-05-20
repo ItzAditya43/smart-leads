94b186954d550d8efd0d4ee213afc9954d5f10b7
ItzAditya43 <adikun10723@gmail.com>
2026-05-20 18:10:36 +0530

    another error

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -15,6 +15,7 @@ const userSchema = new Schema<IUserDocument>(
       minlength: [2, 'Name must be at least 2 characters'],
       maxlength: [50, 'Name cannot exceed 50 characters'],
     },
+
     email: {
       type: String,
       required: [true, 'Email is required'],
@@ -23,12 +24,14 @@ const userSchema = new Schema<IUserDocument>(
       trim: true,
       match: [/^\S+@\S+\.\S+$/, 'Please enter a valid email'],
     },
+
     password: {
       type: String,
       required: [true, 'Password is required'],
       minlength: [6, 'Password must be at least 6 characters'],
       select: false,
     },
+
     role: {
       type: String,
       enum: ['admin', 'sales'] as UserRole[],
@@ -37,9 +40,10 @@ const userSchema = new Schema<IUserDocument>(
   },
   {
     timestamps: true,
+
     toJSON: {
       transform: (_doc, ret) => {
-        delete (ret as any).password;
+        ret.password = undefined;
         return ret;
       },
     },
@@ -47,9 +51,14 @@ const userSchema = new Schema<IUserDocument>(
 );
 
 userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
+  if (!this.isModified('password')) {
+    return next();
+  }
+
   const salt = await bcrypt.genSalt(12);
+
   this.password = await bcrypt.hash(this.password, salt);
+
   next();
 });
 
@@ -59,5 +68,9 @@ userSchema.methods.comparePassword = async function (
   return bcrypt.compare(candidatePassword, this.password);
 };
 
-const User: Model<IUserDocument> = mongoose.model<IUserDocument>('User', userSchema);
+const User: Model<IUserDocument> = mongoose.model<IUserDocument>(
+  'User',
+  userSchema
+);
+
 export default User;
